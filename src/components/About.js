4425581a5425dfb92a4a7e7b7795f6cb8e67f2c6
Stephanie Morrissey <sgmorrissey098@gmail.com>
2020-11-09 10:08:07 +0000

    Added relevant links to connect icons and cv file

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Row, Col, Image } from 'antd';
-import { GithubOutlined } from '@ant-design/icons';
 
 const About = () => {
   return (
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Row, Col } from 'antd';
+import { Row, Col, Button } from 'antd';
 import {
   GithubOutlined,
   LinkedinOutlined,
@@ -12,20 +12,35 @@ const Contact = () => {
     <div>
       <Row align='middle' justify='center'>
         <Col>
-          <GithubOutlined style={{ fontSize: '100px', padding: '5px' }} />
+          <Button type='text' href='https://github.com/steph-morrissey'>
+            <GithubOutlined style={{ fontSize: '100px', padding: '5px' }} />
+          </Button>
         </Col>
         <Col>
-          <LinkedinOutlined
-            style={{ fontSize: '100px', padding: '5px', color: '#0072b1' }}
-          />
+          <Button
+            type='text'
+            href='https://www.linkedin.com/in/stephanie-morrissey-b36170134/'
+          >
+            <LinkedinOutlined
+              style={{ fontSize: '100px', padding: '5px', color: '#0072b1' }}
+            />
+          </Button>
         </Col>
         <Col>
-          <MailOutlined
-            style={{ fontSize: '100px', padding: '5px', color: '#c71610' }}
-          />
+          <Button type='text' href='mailto:sgmorrissey098@gmail.com'>
+            <MailOutlined
+              style={{ fontSize: '100px', padding: '5px', color: '#c71610' }}
+            />
+          </Button>
         </Col>
         <Col>
-          <FileSearchOutlined style={{ fontSize: '100px', padding: '5px' }} />
+          <Button type='text'>
+            <a href='stephanieMorrisseyCV.pdf' download>
+              <FileSearchOutlined
+                style={{ fontSize: '100px', padding: '5px' }}
+              />
+            </a>
+          </Button>
         </Col>
       </Row>
     </div>

d82eca43729706ca368b5ac633e7f40ba3e9640a
Stephanie Morrissey <sgmorrissey098@gmail.com>
2020-11-10 10:46:31 +0000

    contact section

--- src/components/About.js
+++ src/components/About.js
@@ -13,22 +13,11 @@ const About = () => {
       <Col sm={24} md={12} lg={12}>
         <div style={{ backgroundColor: '#FFF', padding: '5px' }}>
           <h1>Hello,</h1>
+          <p>I'm Steph! Welcome to my website.</p>
           <p>
-            I'm Steph, an aspiring Software Developer. I have a graduate degree
-            in BSc Information Technology and Management for Business from the
-            University of Sheffield and have recently completed a Coding
-            Bootcamp course with the University of Manchester. Both of which,
-            have allowed me to create full stack dynamic web applications.
-          </p>
-          <p>
-            Strengths include: ability to learn new skills and programming
-            languages, responsive design, problem solving, Model View Controller
-            (MVC) structure.
-          </p>
-          <p>
-            Experience with: Git/GitHub, HTML, CSS, JavaScript, Node.js,
-            Bootstrap, Tailwind, Foundation Sites, Handlebars, jQuery, Express,
-            React, Sequelize and MongoDB.
+            My specialist software development skills include: HTML5, CSS, CSS
+            Frameworks (Bootstrap, Semantic UI, Tailwind), JavaScript, jQuery,
+            NodeJs, MySQL, MongoDB, Express, Handlebars JS and React JS.
           </p>
         </div>
       </Col>
